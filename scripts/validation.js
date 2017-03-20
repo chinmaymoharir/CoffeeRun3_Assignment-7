53cbf26d352d4ef51f46882c15ea73d8571bbfe6
Chinmay Moharir <chinmay.moharir@gmail.com>
2017-03-20 15:40:34 -0700

    Add files via upload

---
+++ scripts/validation.js
@@ -0,0 +1,18 @@
+(function() {
+    'use strict';
+    var App = window.App || {};
+    var Validation = {
+        isCompanyEmail: function(email) {
+            return /.+@bignerdranch\.com$/.test(email);
+        },
+        isSilverChallenge: function(coffee, strength) {
+            if (/decaf/.test(coffee) && strength > 20) {
+                return false;
+            }
+            return true;
+        }
+    };
+
+    App.Validation = Validation;
+    window.App = App;
+})(window);
